f7e2f1f30711f786f0999a986f79530cca137730
21IZ <wallcroftsempringham@gmail.com>
2024-08-22 19:30:53 +0200

    arreglo de base6

--- src/pages/AdminPanel.js
+++ src/pages/AdminPanel.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
-import React, { useState, useEffect } from 'react';
 import { getMenuItems, addMenuItem, updateMenuItem, deleteMenuItem } from '../utils/storage';
 
 function AdminPanel() {
