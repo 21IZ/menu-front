371953fcd9c263e00d7f8b75c5ec905dfd69e885
21IZ <wallcroftsempringham@gmail.com>
2024-08-22 18:43:05 +0200

    arreglo de base 8

--- src/pages/api/getMenu.js
+++ src/pages/api/getMenu.js
@@ -1,14 +1,6 @@
-import { sql } from '@vercel/postgres';
-
-export default async function handler(req, res) {
+export default function handler(req, res) {
   if (req.method === 'GET') {
-    try {
-      const result = await sql`SELECT * FROM menu_items`;
-      res.status(200).json(result.rows);
-    } catch (error) {
-      console.error('Error fetching menu items:', error);
-      res.status(500).json({ error: 'Error al obtener los ítems del menú' });
-    }
+    res.status(200).json({ message: 'Menu fetched successfully!' });
   } else {
     res.setHeader('Allow', ['GET']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
